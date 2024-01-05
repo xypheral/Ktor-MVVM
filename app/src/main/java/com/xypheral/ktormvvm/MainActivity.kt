763081e2d7285a223b4fdc5eb6b98b00d7fe81bf
Wei Han <noelng26@gmail.com>
2024-01-05 17:35:12 +0800

    updates

--- app/src/main/java/com/xypheral/ktormvvm/MainActivity.kt
+++ app/src/main/java/com/xypheral/ktormvvm/MainActivity.kt
@@ -6,12 +6,14 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.produceState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -34,17 +36,12 @@ class MainActivity : ComponentActivity() {
             )
             KtorMVVMTheme {
                 Surface(color = MaterialTheme.colorScheme.background) {
-                    LazyColumn {
-                        items(posts.value) {
-                            Column(
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .padding(16.dp)
-                            ) {
-                                Text(text = it.title, fontSize = 20.sp)
-                                Spacer(modifier = Modifier.height(4.dp))
-                                Text(text = it.body, fontSize = 14.sp)
-                            }
+                    LazyColumn(
+                        modifier = Modifier.fillMaxSize(),
+                        contentPadding = PaddingValues(16.dp)
+                    ) {
+                        items(posts.value) { post -> // Use posts.value here
+                            PostCard(post = post)
                         }
                     }
                 }
@@ -54,9 +51,32 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun PostCard(post: PostResponse) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(bottom = 8.dp),
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Text(
+                text = "ID: ${post.id}",
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(
+                text = "Title: ${post.title}",
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Normal
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(
+                text = post.body,
+                fontSize = 14.sp,
+                fontWeight = FontWeight.Normal
+            )
+        }
+    }
 }
